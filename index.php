5198a0fadfe8e6c21515e442b51e434f63bd981b
Klainn <klainn@gmail.com>
2012-01-28 14:36:24 -0500

    Rebuild Repo

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Jason Shive</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Cache-Control" content="no-cache">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+<!-- Header of the page, static content -->
+<h1>
+<script language="javascript">  
+function mypopup()
+{
+    mywindow = window.open("includes/login.php", "mywindow", "location=1,status=1,scrollbars=1,  width=500,height=200");
+    mywindow.moveTo(0, 0);
+}
+</script>
+Jason Shive - <A HREF="javascript:mypopup()">Admin Login</A>
+
+</h1>
+<!-- End Header -->
+<!-- Left Menu, static content -->
+<div id="left">
+<?
+	include 'includes/navigation.php';
+?>
+</div>
+<!-- End Left Menu -->
+<!-- Right Menu, static content -->
+<div id="right">
+<a href="http://profile.mygamercard.net/Crunk+Sizzurp"><img src="http://card.mygamercard.net/community/tbs/Crunk+Sizzurp.png" border=0></a>
+<p>Put WoW infoz here.</p>
+</div>
+<!-- End Right Menu -->
+<!-- Main Body Content -->
+<div id="content">
+<h2>CONTENT</h2>
+<p><b>3 columns / menu fixed, content dynamic with head and footer.</b><br />
+
+3 column layout grid. The navigation column are fixed in width, the content column is dynamic and adjusts
+itself to the browser window.</p>
+<p>This layout also works with an absolute height <a href="temp07_100.html">template 100% height</a><br />
+<a href="/">more nice and free css templates</a></p>
+<pre>
+html {
+padding:0px;
+margin:0px;
+}
+body {
+background-color: #e1ddd9;
+font-size: 12px;
+font-family: Verdana, Arial, SunSans-Regular, Sans-Serif;
+color:#564b47;
+padding:0px;
+margin:0px;
+}
+#content {
+margin: 0px 190px 0px 190px;
+border-left: 2px solid #564b47;
+border-right: 2px solid #564b47;
+padding: 0px;
+background-color: #ffffff;
+}
+</pre>
+</div>
+<!-- End Main Body Content -->
+<!-- Footer -->
+<h1>
+in valid  code we trust (*^_^*) miss monorom
+</h1>
+<!-- End Footer -->
+
+</body>
+
+</html>
